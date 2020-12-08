aa7259239a16c4320e7c031f505eed2ed845fdb2
Benjamin Barr <benwbarr@gmail.com>
2020-12-08 15:18:56 -0700

    Working on scrape

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,10 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from webdriver_manager.chrome import ChromeDriverManager
 
-executable_path = {"executable_path": "/Users/sharonsu/Downloads/chromedriver"}
+executable_path = {"executable_path": "/Users/downr/Downloads/chromedriver"}
 browser = Browser("chrome", **executable_path, headless=False)
 
 def scrape():
